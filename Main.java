dec8f87cb44bbc334303f8d09c876f8b56d64953
Sofia C <91104513@g.mvusd.net>
2021-01-29 02:33:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+/* 
+ * Sofia Covarrubias, 01/28/2020, Swing App
+ * This is a duck slide show featuring cool ducks.
+ */
+
+// ----- import statements -----
+import javax.swing.*;
+import java.awt.event.*; //this enables the events
+import java.awt.*;
+
+class Main
+{
+  public static void main (String[] args)
+  {
+    // ----- Frame -----
+
+    // ----- Panel 1 -----
+
+    // ----- Image Label -----
+
+    // ----- Image Text Field -----
+
+    // ----- Adding to Panel 1 -----
+
+    // ----- Panel 2 -----
+
+    // ----- "Next" Button ----- 
+
+    // ----- "Previous" Button -----
+
+    // ----- Adding to Panel 2 -----
+
+    // ----- Adding to Frame -----
+
+  } // end main method
+} // end Main class

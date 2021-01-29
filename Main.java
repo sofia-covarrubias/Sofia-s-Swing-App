b750a1e5b73d291407d091f3d0c9c02754d29fb9
Sofia C <91104513@g.mvusd.net>
2021-01-29 04:04:07 +0000

    Added 2 buttons (prev/next) w/o functionality.

--- Main.java
+++ Main.java
@@ -5,13 +5,23 @@
 
 // ----- import statements -----
 import javax.swing.*;
-import java.awt.event.*; //this enables the events
+import java.awt.event.*; // allows use of events
 import java.awt.*;
+import java.util.ArrayList; // allows use of ArrayList
 
 class Main
 {
   public static void main (String[] args)
   {
+    // ----- Declarations -----
+    ArrayList<String> duckImages = new ArrayList<String>(); // image file names
+    duckImages.add("duck1.jpg");
+    duckImages.add("duck2.jpg");
+    duckImages.add("duck3.jpg");
+    duckImages.add("duck4.jpg");
+    duckImages.add("duck5.jpg");
+    String[] duckNames = {"Goth Girl", "Punk Rocker", "Cool Dude", "Scuba Diver", "Studious Scholar"}; // image label names
+
     // ----- Frame -----
     JFrame frame = new JFrame("Sofia's Duck Slideshow");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // YOU MUST HAVE THIS LINE
@@ -22,22 +32,42 @@ class Main
     JPanel panel1 = new JPanel(new GridLayout(2, 2, 20, 20));
 
     // ----- Image Label -----
-    JLabel imgLabel = new JLabel("Testing");
+    JLabel imgLabel = new JLabel("  Duck #1's Name: ");
 
     // ----- Image Text Field -----
     JTextField imgTextField = new JTextField(10);
 
+    // ----- "Next" Button ----- 
+    JButton nextButton = new JButton("Next →");
+
+    nextButton.addActionListener(new ActionListener()
+    {
+        public void actionPerformed (ActionEvent e)
+        {
+          
+        } //end actionPerformed
+    }); //end ActionListener
+
+    // ----- "Previous" Button ----- 
+    JButton prevButton = new JButton("← Previous");
+
+    prevButton.addActionListener(new ActionListener()
+    {
+        public void actionPerformed (ActionEvent e)
+        {
+          
+        } //end actionPerformed
+    }); //end ActionListener
+
     // ----- Adding to Panel 1 -----
+    panel1.add(prevButton);
+    panel1.add(nextButton);
     panel1.add(imgLabel);
     panel1.add(imgTextField);
 
     // ----- Panel 2 -----
     JPanel panel2 = new JPanel(new BorderLayout(10, 10));
-
-    // ----- "Next" Button ----- 
-
-    // ----- "Previous" Button -----
-
+    
     // ----- Adding to Panel 2 -----
 
     // ----- Adding to Frame -----
